28f59eb35a5a0f3977e792cd9aca041e0b34874a
Dianke kaho <khodianaa@gmail.com>
2026-02-11 19:08:38 +0000

    first commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('index');
+});
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/table', function () {
+    return view('tables');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
